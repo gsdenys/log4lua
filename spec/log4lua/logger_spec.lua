53f02cc79f091aa500ab5ce8ad39a0ee4f9ae46e
Denys G. Santos <gsdenys@gmail.com>
2021-02-24 01:22:22 -0300

    add test for loggeer

---
+++ spec/log4lua/logger_spec.lua
@@ -0,0 +1,57 @@
+-- Copyright 2021 gsdenys. All Rights Reserved.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http:--www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+require 'busted.runner'()
+
+describe("log4lua.logger", function()
+    local level = require 'log4lua.level'
+    local performer = require 'log4lua.performer'
+    
+    describe("level basics", function()
+        local logger = require 'log4lua.logger'
+
+        it("level should exist", function() 
+            assert.truthy(logger.level)
+        end)
+
+        it("level should be INFO", function()
+            assert.are.equals(logger.level, level.INFO)
+        end)
+    end)
+
+    describe("level basics", function()
+        it("performer should exist", function()
+            local logger = require 'log4lua.logger'
+            assert.truthy(logger.performer)
+        end)
+
+        it("performer should be stdout", function()
+            local logger = require 'log4lua.logger'
+            assert.are.equals(logger.performer, performer.stdout)
+        end)
+
+        it("performer should be nginx", function()
+            _G.ngx = {
+                log = function()
+                    -- just a mock function
+                end
+            }
+
+            local logger = require 'log4lua.logger'
+            assert.are.equals(logger.performer, performer.nginx)
+        end)
+    end)
+
+end)
+    
--- src/log4lua/logger.lua
+++ src/log4lua/logger.lua
@@ -13,8 +13,8 @@
 -- limitations under the License.
 
 local level = require 'log4lua.level'
-local performer = require 'performer'
-local helper = require 'helper'
+local performer = require 'log4lua.performer'
+local helper = require 'log4lua.helper'
 
 local logger = {
     --- The default logger level
@@ -35,7 +35,7 @@ function logger:set_level(level_)
 end
 
 --- Perform the log using the selected performer.
-local function logger:log(level_, ...)
+function logger:log(level_, ...)
 
     -- just log if the level has more granularity then the selected log level
     if level.value < self.level.value then
@@ -48,26 +48,26 @@ end
 
 --- Log informations that is diagnostically helpful to people more than just 
 --- developers (IT, sysadmins, etc.).
-local function logger:debug(...)
+function logger:debug(...)
     self.log(level.DEBUG, {...})
 end
 
 --- Log useful information to log (service start/stop, configuration assumptions, etc). 
 --- Info is always available but usually don't care about under normal circumstances. 
 --- This is the out-of-the-box config level.
-local function logger:info(...)
+function logger:info(...)
     self.log(level.INFO, {...})
 end
 
 --- Log useful information to log that should be noted (what port the server is using). 
 --- Notice is always available but may be is useful under normal circumstances. 
-local function logger:notice(...)
+function logger:notice(...)
     self.log(level.NOTICE, {...})
 end
 
 --- Log anything that can potentially cause application oddities, but for which 
 --- automatically recovering.
-local function logger:warn(...)
+function logger:warn(...)
     self.log(level.WARN, {...})
 end
 
@@ -75,13 +75,15 @@ end
 --- (can't open a required file, missing data, etc.). These errors will force user
 --- (administrator, or direct user) intervention. These are usually reserved for 
 --- incorrect connection strings, missing services, etc.
-local function logger:error(...)
+function logger:error(...)
     self.log(level.ERROR, {...})
 end
 
 --- Log any error that is forcing a shutdown of the service or application to prevent 
 --- data loss (or further data loss). It's reserved only for the most heinous errors 
 --- and situations where there is guaranteed to have been data corruption or loss.
-local function logger:crit(...)
+function logger:crit(...)
     self.log(level.CRIT, {...})
 end
+
+return logger
